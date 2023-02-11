52908c2930835cfd35bdfeb08997167a3ad8b569
Joao Pedro <jpcsnoberto@gmail.com>
2023-02-11 00:18:39 -0300

    user input, canvas paint and color selection

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
 const canvas = document.querySelector('#canvas')
-const dimension = 4
-fillCanvas(dimension) 
+const buttonSetCanvas = document.querySelector('#setCanvasSize')
+const colorInput = document.querySelector('#colorInput')
+/* const dimension = 4 */
+/* fillCanvas(dimension) */
+let selectedColor = colorInput.value
 
+colorInput.addEventListener('change', () =>{
+    selectedColor = colorInput.value
+})
+
+buttonSetCanvas.addEventListener('click', () =>{
+    const canvasDimension = getUserInput()
+    fillCanvas(canvasDimension)
+})
+
+canvas.addEventListener('mouseover', (e) =>{
+    if(canvas.hasChildNodes()){
+        paintCanvas(e.target)
+    }
+})
+
+function getUserInput(){
+    let canvasDimension
+
+    while(true){
+        canvasDimension = Number(window.prompt('Insert the canvas dimension (max. 64):'))
+
+        if(canvasDimension.valueOf() < 0 || canvasDimension.valueOf() > 64 ||
+        !Number.isInteger(canvasDimension.valueOf()) || isNaN(canvasDimension.valueOf())){
+            alert('Insert a valid value')
+        }
+        else{
+            break
+        }    
+    }
+    return canvasDimension.valueOf()
+}
 
 function fillCanvas(dimension){
+    canvas.replaceChildren()
     let canvasSquareCount = dimension*dimension
     let squareDimension = (57.6/dimension)
     console.log(squareDimension)
@@ -17,4 +52,8 @@ function fillCanvas(dimension){
         div.style.cssText += `width: ${squareDimension}rem; height: ${squareDimension}rem;`
         canvas.appendChild(div)
     }
+}
+
+function paintCanvas(target){
+    target.style.backgroundColor = `${selectedColor}`
 }
